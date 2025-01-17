dec01cbdafb27bf0ecfd34dd90b355d37717dcc8
Fatemeh Mahabadi <144953741+fa-mahabadi@users.noreply.github.com>
2025-01-17 11:53:50 +0330

    add | user file

---
+++ user.py
@@ -0,0 +1,15 @@
+class user:
+    accounts={}
+        
+    def create_account(self):
+        self.username=username
+        self.password=password
+        accounts[username]=password
+    def authentication(self,username,password):
+        return accounts==username and accounts['username']==password
+    def modify_account(self,new_password):
+        if username in accounts:
+            accounts[username]=new_password
+    
+        
+
