784afdd9d5e9470ac5a3f0548c0077b87bfeb2eb
Fatemeh Mahabadi <144953741+fa-mahabadi@users.noreply.github.com>
2025-01-17 22:19:08 +0330

    Update | user.py

--- user.py
+++ user.py
@@ -1,15 +1,43 @@
-class user:
-    accounts={}
-        
-    def create_account(self):
-        self.username=username
-        self.password=password
-        accounts[username]=password
-    def authentication(self,username,password):
-        return accounts==username and accounts['username']==password
-    def modify_account(self,new_password):
-        if username in accounts:
-            accounts[username]=new_password
-    
-        
+import pickle
 
+class User:
+    accounts = {}
+
+    @classmethod
+    def save_users(cls):
+        '''save users in pickle file '''
+        with open("data/users.pkl", "wb") as f:
+            pickle.dump(cls.accounts, f)
+
+    @classmethod
+    def load_users(cls):
+        '''load users from file '''
+        try:
+            with open("data/users.pkl", "rb") as f:
+                cls.accounts = pickle.load(f)
+        except FileNotFoundError:
+            cls.accounts={}
+
+    @classmethod
+    def create_account(cls, username, password):
+        '''create account'''
+        cls.accounts[username] = password
+        cls.save_users()
+
+    @classmethod
+    def authentication(cls, username, password):
+        '''authenticate user '''
+        cls.load_users()
+        if username in cls.accounts and cls.accounts[username] == password:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def modify_account(cls,username, new_password):
+        '''modify account user '''
+        if username in cls.accounts:
+            cls.accounts[username] = new_password
+            cls.save_users()
+
+    
