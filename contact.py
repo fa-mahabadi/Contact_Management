188c16a7f0a2a6c10aa5f0af394096371d3bbb97
Fatemeh Mahabadi <144953741+fa-mahabadi@users.noreply.github.com>
2025-01-17 22:18:28 +0330

    Update | contact.py

--- contact.py
+++ contact.py
@@ -1,21 +1,48 @@
-class Contact:
-    all_contact={}
-    
-    def add_concat(self,name,email,phone):
-        Contact.all_contact[name]={'email':email,"phone":phone}
-    def delete_contact(self,name):
-        if name in Contact.all_contact:
-            Contact.all_contact.pop(name)
-    def edit_contact(self,name,new_email,new_phone):
-        try:
-            if name in Contact.all_contact:
-                Contact.all_contact[name]={"email":new_email,"phone":new_phone}
-            print(Contact.all_contact)
-        except:
-            print("user not exist")
+import pickle
 
-c=Contact()
-c.add_concat("a","a@yahoo.com","123456")
-c.add_concat("b","b@yahoo.com","123466")
-# c.delete_contact("a")
-c.edit_contact("a","c@yahoo.com","123456")
+
+class Contact:
+    all_contact = {}
+
+    @classmethod
+    def save_contacts(cls):
+        """save contact to pickle file"""
+        with open("data/contacts.pkl", "wb") as f:
+            pickle.dump(cls.all_contact, f)
+
+    @classmethod
+    def load_contacts(cls):
+        """load contact from pickle file"""
+        with open("data/contacts.pkl", "rb") as f:
+            cls.all_contact = pickle.load(f)
+
+    @classmethod
+    def add_contact(cls, name, email, phone):
+        """add new contact"""
+        cls.all_contact[name] = {"email": email, "phone": phone}
+        cls.save_contacts()
+
+    @classmethod
+    def delete_contact(cls, name):
+        """delete a contact"""
+        if name in cls.all_contact:
+            cls.all_contact.pop(name)
+            cls.save_contacts()
+
+    @classmethod
+    def edit_contact(cls, name, new_email, new_phone):
+        """edit existing contact"""
+        if name in cls.all_contact:
+            cls.all_contact[name] = {"email": new_email, "phone": new_phone}
+            cls.save_contacts()
+        else:
+            print(f"{name} not exist")
+
+    @classmethod
+    def diaplay_all_contact(cls):
+        """display all contact from file"""
+        cls.load_contacts()
+        for name, details in cls.all_contact.items():
+            email = details["email"]
+            phone = details["phone"]
+            print(f"Name: {name}, Email: {email}, Phone: {phone}")
